b20c2f2571ca61667fe1d395f677ebdcbbc9dd2f
DESKTOP-NDGE3EJ\CarlosLap <condesa.sys@gmail.com>
2019-02-25 18:08:00 -0600

    Initial and clean commit

---
+++ TsVendingMachine/js/app.js
@@ -0,0 +1,9 @@
+"use strict";
+var VendingMachine = /** @class */ (function () {
+    function VendingMachine() {
+    }
+    return VendingMachine;
+}());
+/// <reference path="vendingMachine.ts" />
+var machine = new VendingMachine();
+//# sourceMappingURL=app.js.map
